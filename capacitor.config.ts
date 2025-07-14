eef0832f0132396f648cafa27f0c26747b1f029f
alokRajEyantrik <alok.kumar@eyantrik.com>
2025-07-14 16:30:39 +0530

    setup initialized

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.formnative.app',
+  appName: 'formnative',
+  webDir: 'dist'
+};
+
+export default config;
